0a7bf4dee845e1f13252e6fa53d799880515da77
janenyasoro <janenyasoro@gmail.com>
2023-10-12 07:46:07 -0700

    More work on python data structures

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
+void print_python_list(PyObject *p)
+{
+	printf([*] Python list info\n);
+}
+
+/**
+ * print_python_bytes - Prints basic info about Python byte objects.
+ * @p: A PyObject byte object.
+ */
+void print_python_bytes(PyObject *p)
+{
+	printf("\n")
+}
